09750acea91e4c6993b7fb15086e3586928bb797
Kynderman Baraldi <kynderman@gmail.com>
2023-05-01 21:34:54 -0300

    feat: add views crud

--- livro/urls.py
+++ livro/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.buscar, name='buscar'),
-    path('listar/', views.listar, name='listar'),
-    path('emprestimo/', views.emprestimo, name='emprestimo'),
-    path('emprestar/', views.emprestar, name='emprestar'),
-    path('novo/', views.novo, name='cadastrarLivro'),
+    path('estoque/', views.estoque, name='estoque'),
+    path('historico/', views.historico, name='historico'),
+    path('novo/', views.novo, name='novo'),
+    path('apagar/<int:livro_id>/', views.apagar, name='apagar'),
+    path('emprestar/<int:livro_id>/', views.emprestar, name='emprestar'),
     path('cadastro/<int:livro_id>/', views.cadastrar, name='cadastrarLivro'),
     path('livro/<int:livro_id>/', views.detalhes_livro, name='detalhes_livro'),
-    # path('fake/', views.fake, name='fake')
 ]
--- livro/views.py
+++ livro/views.py
@@ -1,6 +1,7 @@
+import datetime
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
-from livro.models import LivroForm, Livros
+from livro.models import Emprestimo, LivroForm, Livros
 
 from usuarios.models import Usuario
 # Create your views here.
@@ -34,23 +35,50 @@ def buscar(request):
 
 
 def cadastrar(request, livro_id=None):
-    livro = None
 
-    if livro_id:
-        livro = get_object_or_404(Livros, pk=livro_id)
+    livro = None
+    try:
+        if livro_id:
+            livro = get_object_or_404(Livros, pk=livro_id)
+        usuario = Usuario.objects.get(id=request.session.get('usuario'))
+    except:
+        usuario = None
 
     if request.method == 'POST':
         form = LivroForm(request.POST, instance=livro)
         if form.is_valid():
             form.save()
-            return render(request, 'livro_sucesso.html')
+            return render(request, 'livro_sucesso.html', {'usuario': usuario})
     else:
         form = LivroForm(instance=livro)
 
-    return render(request, 'livro_form.html', {'form': form})
+    return render(request, 'livro_form.html', {'usuario': usuario, 'form': form})
 
 
 def detalhes_livro(request, livro_id):
+
+    try:
+        livro = get_object_or_404(Livros, pk=livro_id)
+        livro_anterior = Livros.objects.filter(
+            id__lt=livro.id).order_by('-id').first()
+        proximo_livro = Livros.objects.filter(
+            id__gt=livro.id).order_by('id').first()
+        usuario = Usuario.objects.get(id=request.session.get('usuario'))
+
+        context = {
+            'usuario': usuario,
+            'livro': livro,
+            'livro_anterior': livro_anterior,
+            'proximo_livro': proximo_livro,
+        }
+
+    except:
+        context = None
+
+    return render(request, 'detalhes_livro.html', context=context)
+
+
+def apagar(request, livro_id):
 
     try:
         livro = get_object_or_404(Livros, pk=livro_id)
@@ -58,62 +86,65 @@ def detalhes_livro(request, livro_id):
     except:
         usuario = None
 
-    return render(request, 'detalhes_livro.html', {'usuario': usuario, 'livro': livro})
+    if request.method == 'POST':
+        livro.delete()
+        return render(request, 'apagado_sucesso.html', {'usuario': usuario})
+
+    return render(request, 'apagar.html', {'usuario': usuario, 'livro': livro})
 
 
-def emprestimo(request):
-    return HttpResponse('emprestimo livro')
+def emprestar(request, livro_id):
+
+    try:
+        livro = get_object_or_404(Livros, pk=livro_id)
+        usuario = Usuario.objects.get(id=request.session.get('usuario'))
+    except:
+        usuario = None
+
+    if request.method == 'POST':
+        usuario_id = request.POST.get('usuario_id')
+        data_emprestimo = datetime.now().date()
+        data_devolucao = data_emprestimo + datetime.timedelta(days=7)
+
+        emprestimo = Emprestimo(livro=livro, usuario_id=usuario_id,
+                                data_emprestimo=data_emprestimo, data_devolucao=data_devolucao)
+        emprestimo.save()
+
+        livro.disponivel -= 1
+        livro.save()
+
+        return render(request, 'emprestimo_sucesso.html', {'usuario': usuario})
+
+    return render(request, 'emprestimo.html', {'usuario': usuario, 'livro': livro})
 
 
-def emprestar(request):
-    return HttpResponse('emprestar livro')
+def estoque(request):
+    try:
+        usuario = Usuario.objects.get(id=request.session.get('usuario'))
+        livros = Livros.objects.all()
+
+    except:
+        usuario = None
+        livros = None
+
+    return render(request, 'estoque.html', {'usuario': usuario, 'livros': livros})
 
 
-def listar(request):
-    return HttpResponse('listar livro')
+def historico(request):
+    try:
+        usuario = Usuario.objects.get(id=request.session.get('usuario'))
+
+        if usuario.is_admin:
+            emprestimos = Emprestimo.objects.all()
+        else:
+            emprestimos = Emprestimo.objects.filter(usuario=usuario)
+
+    except:
+        usuario = None
+        emprestimos = None
+
+    return render(request, 'historico.html', {'usuario': usuario, 'emprestimos': emprestimos})
 
 
 def novo(request):
     return cadastrar(request)
-
-
-def fake(request):
-
-    livros = [
-        {'titulo': 'O Labirinto de ossos', 'autor': 'Rick Riordan', 'editora': 'Intrínseca',
-            'isbn': '978-85-8057-032-4', 'categoria': 'Ficção infantojuvenil', 'quantidade': 10},
-        {'titulo': 'As Crônicas de Nárnia: O Leão, A Feiticeira e o Guarda-Roupa', 'autor': 'C.S. Lewis',
-            'editora': 'WMF Martins Fontes', 'isbn': '978-85-7827-271-6', 'categoria': 'Ficção infantojuvenil', 'quantidade': 8},
-        {'titulo': 'A Revolução dos Bichos', 'autor': 'George Orwell', 'editora': 'Companhia das Letras',
-            'isbn': '978-85-359-3226-0', 'categoria': 'Ficção distópica', 'quantidade': 15},
-        {'titulo': 'O Código Da Vinci', 'autor': 'Dan Brown', 'editora': 'Arqueiro',
-            'isbn': '978-85-8041-674-9', 'categoria': 'Ficção de suspense', 'quantidade': 20},
-        {'titulo': 'A Arte da Guerra', 'autor': 'Sun Tzu', 'editora': 'L&PM Pocket',
-            'isbn': '978-85-254-1721-1', 'categoria': 'Filosofia e estratégia militar', 'quantidade': 5},
-    ]
-
-    livros = [
-        {'titulo': 'O Sol é Para Todos', 'autor': 'Harper Lee', 'editora': 'Editora José Olympio',
-            'isbn': '978-85-03-00194-7', 'categoria': 'Ficção', 'quantidade': 12},
-        {'titulo': '1984', 'autor': 'George Orwell', 'editora': 'Companhia das Letras',
-            'isbn': '978-85-359-0277-6', 'categoria': 'Ficção distópica', 'quantidade': 7},
-        {'titulo': 'A Sangue Frio', 'autor': 'Truman Capote', 'editora': 'Companhia das Letras',
-            'isbn': '978-85-359-1241-3', 'categoria': 'Não-ficção', 'quantidade': 3},
-        {'titulo': 'O Conde de Monte Cristo', 'autor': 'Alexandre Dumas', 'editora': 'Penguin-Companhia',
-            'isbn': '978-85-250-0031-7', 'categoria': 'Ficção', 'quantidade': 20},
-        {'titulo': 'A Hora da Estrela', 'autor': 'Clarice Lispector', 'editora': 'Rocco',
-            'isbn': '978-85-325-0328-0', 'categoria': 'Ficção', 'quantidade': 15}
-    ]
-
-    for livro in livros:
-        novo_livro = Livros.objects.create(
-            titulo=livro['titulo'],
-            autor=livro['autor'],
-            editora=livro['editora'],
-            isbn=livro['isbn'],
-            categoria=livro['categoria'],
-            quantidade=livro['quantidade']
-        )
-        novo_livro.save()
-
-    return HttpResponse('Livros cadastrados com sucesso!')
